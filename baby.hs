02850835e6c9371f770857fb56ddb3ce8255bc55
Declan Quinn <quinndeclan00@gmail.com>
2022-08-05 14:28:07 +0100

    initial commit

---
+++ baby.hs
@@ -0,0 +1,6 @@
+doubleMe x = x + x
+doubleUs x y = x + x + y + y
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
